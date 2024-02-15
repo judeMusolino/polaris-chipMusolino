a1a6ab9d9d3bb9361f1be5d6551b5943f4c35004
judeMusolino <jnm5704@psu.edu>
2024-02-15 15:50:18 -0500

    office hours work
    got the confetti working. Everything should be good.

--- src/counter-app.js
+++ src/counter-app.js
@@ -19,14 +19,17 @@ export class CounterApp extends LitElement {
           display: inline-flex; 
         }
 
-        :host([counter=16]).count {
-            color: red; 
+        #confetti {
+            height: 100%; 
+            width: 100%; 
         }
 
         .wrapper {
-            background-color: yellow; 
+            background-color: khaki; 
             width: 180px; 
             height: 160px;  
+            box-shadow: 5px 5px 5px brown;
+            margin: 8px;  
         }
 
         .count {
@@ -49,30 +52,33 @@ export class CounterApp extends LitElement {
             font-size: 32px; 
             margin: 8px;
             width: 40px; 
-
         }
 
         .plus:hover, .plus:focus, .minus:hover, .minus:focus {
             background-color: black; 
             color: white; 
         }
-
         `;
     }
 
     render() {
+        var textColor = "black";
+        if(this.counter === 18) {textColor = "blue"}
+        if(this.counter === 21) {textColor = "magenta"}
+        if(this.counter === this.min) {textColor = "brown"}
+        if(this.counter === this.max) {textColor = "brown"}
+
+
       return html`
         <div class="wrapper">
-        <div class="count">${this.counter}
-        </div>
-        <div class="min">${this.min}
-        </div>
-        <div class="max">${this.max}
+        <confetti-container id="confetti">
+        <div class="count" style="color:${textColor}">${this.counter}
         </div>
         <div class="buttonwrap">
         <button class="minus" @click="${this.decrease}" ?disabled="${this.min === this.counter}">-</button>
         <button class="plus" @click="${this.increase}" ?disabled="${this.max === this.counter}">+</button>
         </div>
+        </confetti-container>
         </div> 
         `;
     }
@@ -87,6 +93,23 @@ export class CounterApp extends LitElement {
         if (this.counter < this.max) {
             this.counter++;
         }
+    }
+
+    
+    updated(changedProperties) {
+        if (changedProperties.has("counter") && this.counter == 21) {
+            this.makeItRain(); 
+        }
+    }
+  
+    makeItRain() {
+        import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
+        (module) => {
+            setTimeout(() => {
+            this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+            }, 0);
+        }
+        );
     }
 
     static get properties() {
